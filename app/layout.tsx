3a4859401d54c9f744898ebc42529588a3e0bff6
ale700-suno <artyushkovasasha7@gmail.com>
2026-06-03 14:09:42 +0300

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata, Viewport } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import { GoogleAnalytics } from "@/components/GoogleAnalytics";
+import Script from "next/script";
 import "./globals.css";
 
 const siteUrl = "https://mil-contract-rf.vercel.app";
@@ -81,6 +82,32 @@ export default function RootLayout({
     >
       <head>
         <GoogleAnalytics />
+
+        {/* Yandex Metrika */}
+        <Script
+          id="yandex-metrika"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(m,e,t,r,i,k,a){
+                m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+                m[i].l=1*new Date();
+                for (var j = 0; j < document.scripts.length; j++) {
+                  if (document.scripts[j].src === r) { return; }
+                }
+                k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)
+              })(window, document,'script','https://mc.yandex.ru/metrika/tag.js?id=109612757', 'ym');
+              ym(109612757, 'init', {
+                ssr:true,
+                webvisor:true,
+                clickmap:true,
+                ecommerce:"dataLayer",
+                accurateTrackBounce:true,
+                trackLinks:true
+              });
+            `,
+          }}
+        />
       </head>
       <body className="min-h-screen bg-black text-white antialiased overflow-x-hidden select-none">
         {children}
